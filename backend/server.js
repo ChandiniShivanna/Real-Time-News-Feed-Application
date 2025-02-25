631ea95581727494660592005dfa217f1e595453
chandini1915 <chandini.s@gaiansolutions.com>
2025-02-25 22:03:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -53,16 +53,8 @@ app.use((req, res, next) => {
 app.use("/api/news", newsRoutes);
 
 // Connect to MongoDB
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .connect(MONGO_URI)
+  .then(() => console.log(" Connected to MongoDB"))
+  .catch((error) => console.error(" MongoDB Connection Error:", error));
